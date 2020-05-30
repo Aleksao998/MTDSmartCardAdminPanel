1ea826636d561f962b52d8cefea34ec3fe98b1eb
AleksaOpacic <aleksaopacic988@gmail.com>
2020-05-31 00:25:43 +0200

    Updates

---
+++ precache-manifest.d58cb79c55c91dd9e3b1b939a599e40b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a0b005f8901ec3b4c08d4985c6c7e5",
+    "url": "/MTDSmartCardAdminPanel/index.html"
+  },
+  {
+    "revision": "31b1fe8ae38077d54af2",
+    "url": "/MTDSmartCardAdminPanel/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "4a617db833d4feb4979f",
+    "url": "/MTDSmartCardAdminPanel/static/css/main.f67ad7ff.chunk.css"
+  },
+  {
+    "revision": "31b1fe8ae38077d54af2",
+    "url": "/MTDSmartCardAdminPanel/static/js/2.0d5f828c.chunk.js"
+  },
+  {
+    "revision": "4a617db833d4feb4979f",
+    "url": "/MTDSmartCardAdminPanel/static/js/main.a39aae60.chunk.js"
+  },
+  {
+    "revision": "f6a2b5f06a6df61d3aac",
+    "url": "/MTDSmartCardAdminPanel/static/js/runtime~main.7f2ad8dc.js"
+  },
+  {
+    "revision": "70112bc0a10966fe177746c4e69a46b5",
+    "url": "/MTDSmartCardAdminPanel/static/media/card.70112bc0.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/MTDSmartCardAdminPanel/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
